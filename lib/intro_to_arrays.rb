2cc8160c61e4bf46ad014ce510bfae75567435d7
Maija Barrins <maija.barrins@gmail.com>
2019-04-25 19:40:08 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,21 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  [1, 2]
+end
+
+def first_element(arr)
+  #arr[0]
+  arr.first
+end
+
+def third_element(arr)
+  arr[2]
+end
+
+def last_element(arr)
+  #arr[-1]
+  arr.last
+end
